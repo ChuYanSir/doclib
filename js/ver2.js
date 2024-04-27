fc106030cb0b1252b0b8bfaec38c8f9c71760b8f
ChuYanSir <chuyansir@163.com>
2024-04-27 23:43:52 +0800

    更新文件

---
+++ js/ver2.js
@@ -0,0 +1,8 @@
+console.log(btjson_ver);
+if(btjson_ver == "1.0.9"){
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/ChuYanSir/doclib/js/bofangqi/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/ChuYanSir/doclib/js/bofangqi/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/ChuYanSir/doclib/js/bofangqi/js/btjson1.0.9.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/ChuYanSir/doclib/js/bofangqi/js/btjsonplayer1.0.9.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/ChuYanSir/doclib/js/bofangqi/js/layer.min.js'></script>");
+}
